c95e57e1610cc278cf9a43b25bddb594a55a7410
Nelson Kobayashi <koba.nelson@gmail.com>
2023-03-26 19:11:23 -0300

    subiu exercício aula 4

---
+++ Aula4 Primeiro Endpoint/src/exercicio.ts
@@ -0,0 +1,23 @@
+/* import express from 'express';
+import { Request, Response, Router } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    const healthCheck = {message: "O exercício está indo de vento em polpa"};
+    res.send(healthCheck);
+})
+
+app.use(router);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Exercício rodando normalmente no endereço http://localhost:${port}`);
+}); */
